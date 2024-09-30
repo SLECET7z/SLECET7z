bbc1f27677c569546f5d0d67b64a8052b2894f90
SLECET7z <iyedraif304@gmail.com>
2024-09-30 21:19:38 +0100

    Update and rename SS.JS to Uesr.js

--- Uesr.js
+++ Uesr.js
@@ -3,13 +3,13 @@ function getUserCredentials() {
     var correctPassword = "1DAYfree";  // كلمة المرور الصحيحة
 
     while (true) {
-        var username = prompt('ENTER USERNAME:");
-        var password = prompt("ENTER PASSWORD:");
+        var username = prompt("ENTER USERNAME:");
+        var password = prompt("ENTER PASSWORD");
 
         if (username && password) {
             if (username === correctUsername && password === correctPassword) {
-                alert("welcome to panel SLECET7z (1DAY)");
-                break; // الخروج من الحلقة إذا كانت البيانات صحيحة
+                alert(‘welcome to panel SLECET7z (1DAY)’);
+                break; // الخروج من الحلقة إذا كانت المدخلات صحيحة
             } else {
                 alert("PASSWORD OR USERNAME INCORRECT");
             }
